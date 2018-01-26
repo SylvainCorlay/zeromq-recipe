10b1579ba3ad8d4b9b67baf63eb9ac94200ff5a6
Johan Mabille <johan.mabille@gmail.com>
2018-01-26 17:08:15 +0100

    copy of dll on windows (required by pyzmq)

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,11 @@ if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 
+copy /y %LIBRARY_BIN%\libzmq-mt-4*.dll /b %LIBRARY_BIN%\libzmq.dll		
+if errorlevel 1 exit 1		
+copy /y %LIBRARY_LIB%\libzmq-mt-4*.lib /b %LIBRARY_LIB%\libzmq.lib		
+if errorlevel 1 exit 1
+ 
 .\bin\test_ancillaries
 .\bin\test_atomics
 .\bin\test_base85
